785c3de03630ca44a65c3c527776ce564ae4eb03
honki12345 <iui4793@gmail.com>
2026-02-24 10:16:11 +0900

    feat: 위키 상위 경로 버튼 목적지 라벨 노출

--- tests/ui/accessibility.spec.ts
+++ tests/ui/accessibility.spec.ts
@@ -14,6 +14,7 @@ test("critical pages have no serious axe violations", async ({
   const loggedOutTargets = [
     "/",
     "/wiki",
+    "/wiki/sample",
     `/wiki/${seeded.wikiPath}`,
     "/posts",
     `/posts/${seeded.detailSlug}`,
